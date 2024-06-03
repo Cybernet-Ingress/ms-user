f60b0d741104b8572c6751b972b5e5e24089d45f
mahmud.fatullayev <mahmud.fatullayev@cybernet.az>
2024-06-03 19:16:15 +0400

    bugfix some methods

---
+++ src/main/java/com/example/msuser/model/request/AuthRequest.java
@@ -0,0 +1,15 @@
+package com.example.msuser.model.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AuthRequest {
+    private String mail;
+    private String password;
+}
